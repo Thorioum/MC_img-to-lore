3d27b2d08e8f814eaf19c916bed6612819c352fd
Thorioum <98676446+Thorioum@users.noreply.github.com>
2023-03-05 17:51:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import pyperclip
 def rgb_to_hex(rgb):
     return '%02x%02x%02x' % rgb
 def rgb_of_pixel(img_path, x, y):
@@ -11,7 +12,7 @@ item = input()
 print("type the full path to the image you would like to use (Ex: C:\picture\example.jpg, __NO PNGS__):")
 path = input()
 img = Image.open(path, 'r')
-print("please specify the size of the image you want to scale it to (keeping it below 2000 total pixels is a good guide):")
+print("please specify the size of the image you want to scale it to (keeping it below 2000 total pixels is a good guide)\n RES 106x47 will fill an entire 1920x1080 gui scale 2 minecraft screen:")
 print("type width: ")
 w = input()
 print("type height: ")
@@ -19,35 +20,47 @@ h = input()
 image = img.resize((int(w), int(h)), Image.Resampling.LANCZOS)
 pix = image.load()
 image.show();
-command = "/give @p " + item + "{display:{Lore:["
+command = "{display:{Lore:["
 i = 0
 j = 0
 ran = 0
+isPng = 0 #checks if the image ends in png
+if "png" in path:
+    isPng = 1
 while i < image.height:
     command += "\'"
     command += "["
     while j < image.width:
-        r,g,b = pix[j,i]
-        rgb = r,g,b
-        hexval = rgb_to_hex(rgb)
+        if isPng == 1:
+            r,g,b,a = pix[j,i]
+            rgb = r,g,b
+            hexval = rgb_to_hex(rgb)
+        else:
+            r,g,b = pix[j,i]
+            rgb = r,g,b
+            hexval = rgb_to_hex(rgb)
 
         squares = "█"
         while(1) :
             if(j==image.width-1) : #stops index out of range exceptions
                 break;
-            r,g,b = pix[j,i]
-            r1,g1,b1 = pix[j+1,i]
-            rgb = r,g,b
-            rgb2 = r1,g1,b1
+            if isPng == 1:
+                r,g,b,a = pix[j,i]
+                r1,g1,b1,a1 = pix[j+1,i]
+                rgb = r,g,b
+                rgb2 = r1,g1,b1
+            else:
+                r,g,b = pix[j,i]
+                r1,g1,b1 = pix[j+1,i]
+                rgb = r,g,b
+                rgb2 = r1,g1,b1
             if(rgb_to_hex(rgb) == rgb_to_hex(rgb2)):
                 j+=1
                 squares += "█" #if the pixel next to it is the same color it doesnt create a new text parameter and instead adds another square
             else:
                 break
 
-
         command += ("{\"text\":\"" + squares + "\",\"color\":\"#" + hexval + "\"") #adds the text with the hex value
-
         if ran == 0:
             command += ",\"italic\":false" # sets the first text on the line to not italic
         ran = 1
@@ -65,6 +78,7 @@ while i < image.height:
     i+=1
     j=0
     ran = 0
-
-command += "]}} 1"
-print(command)
+command += "]}}"
+pyperclip.copy(command)
+print("/give @p " + item + command)
+print("nbt data copied to clipboard")
